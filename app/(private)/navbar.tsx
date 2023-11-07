620fe3ed24b158821ecc595d5fdd73642c8656de
Rayan Ramazan <rayanramazan09@gmail.com>
2023-11-07 05:13:48 +0300

    highlight active link

--- app/(private)/navbar.tsx
+++ app/(private)/navbar.tsx
@@ -1,18 +1,27 @@
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 
 export default function NavBar() {
+    const pathName = usePathname();
     return (
         <ul className=" bg-slate-600 flex mt-3 justify-between items-center p-4 rounded text-white">
-            <li>
+            <li
+                className={pathName.startsWith("/feed") ? "text-green-400" : ""}
+            >
                 <Link href="/feed">Feed</Link>
             </li>
-            <li>
+            <li
+                className={pathName.startsWith("/profile") ? "text-green-400" : ""}>
                 <Link href="/profile">Profile</Link>
             </li>
-            <li>
+            <li
+                className={pathName.startsWith("/following") ? "text-green-400" : ""}
+            >
                 <Link href="/following">Following</Link>
             </li>
-            <li>
+            <li
+                className={pathName.startsWith("/followers") ? "text-green-400" : ""}
+            >
                 <Link href="/followers">Followers</Link>
             </li>
         </ul>
