9a4d92d41975bd583e62fc50d9f6773c4c4508a9
Rayan Ramazan <rayanramazan09@gmail.com>
2023-11-07 04:20:08 +0300

    swr and fetcher

--- app/(private)/header.tsx
+++ app/(private)/header.tsx
@@ -1,5 +1,27 @@
+"use client";
+import useSWR from "swr";
+
+const fetcher = async (url: RequestInfo | URL) => {
+    const res = await fetch(url);
+    if(!res.ok){
+        const msg = "An error occured while fetching data.";
+        const info = await res.json();
+        const statue = res.status;
+        const error = new Error(msg);
+        console.error(info, statue);
+        throw error;
+    }
+    return res.json();
+}
+
 export default function Header() {
+    const { data, error, isLoading } = useSWR("/api/user/profile", fetcher)
+
+    if(error) return <div>{error.message}</div>
+    if(isLoading) return <div>Loading...</div>
+
+    console.log(data);
     return (
-        <header>Header</header>
+        <header>{data.data.username}</header>
     )
 }
