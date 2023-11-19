296b4fbcc4873d7e8f56b203bbe3e64274777b60
Rayan Ramazan <rayanramazan09@gmail.com>
2023-11-19 04:24:41 +0300

    get post endpoint

---
+++ app/api/post/[id]/route.ts
@@ -0,0 +1,16 @@
+import { getJWTPayload } from "@/app/util/auth";
+import { sql } from "@/db";
+import { NextResponse } from "next/server";
+
+export async function GET(
+    requiest: Request,
+    { params }: { params: {id: number }}
+    ) {
+    const jwtPayload = await getJWTPayload();
+    const res = await sql("select * from posts where id = $1 and user_id = $2",
+    [params.id, jwtPayload.sub,]);
+    if(res.rowCount == 0 ) {
+        return NextResponse.json({error: "Post not found"} , { status: 404 });
+    }
+    return NextResponse.json({ data: res.rows[0]});
+}
