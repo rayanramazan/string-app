f70f460702b579744b00b92ff41aa0fe3dce1b52
Rayan Ramazan <rayanramazan09@gmail.com>
2023-11-19 04:36:54 +0300

    update post endpoint

--- app/api/post/[id]/route.ts
+++ app/api/post/[id]/route.ts
@@ -14,3 +14,21 @@ export async function GET(
     }
     return NextResponse.json({ data: res.rows[0]});
 }
+
+export async function PATCH(
+    requiest: Request,
+    { params }: {params: {id: number}} 
+) {
+    const body = await requiest.json();
+    const jwtPayload = await getJWTPayload();
+    const res = await sql("select * from posts where id = $1 and user_id = $2",
+    [params.id, jwtPayload.sub,]);
+
+    if(res.rowCount == 0) {
+        return NextResponse.json({ error: "Post not found" } , { status: 404 });
+    }
+    await sql("update posts set content = $1 where user_id = $2 and id = $3",
+    [body.content, jwtPayload.sub, params.id]);
+    return NextResponse.json({ data: "update success"});
+
+}
