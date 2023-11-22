256f79c0db47b2f7f1755cf2b2ce26295c5e3548
Rayan Ramazan <rayanramazan09@gmail.com>
2023-11-22 08:04:02 +0300

    search bar. install lodash. debounce search

--- app/(private)/search-bar.tsx
+++ app/(private)/search-bar.tsx
@@ -1,8 +1,11 @@
+import * as _ from "lodash";
 import { ChangeEvent, useState } from "react";
 
 export default function SearchBar() {
 
     const [searchResults, setSearchResults] = useState([]);
+
+    const debouncedFetchSearchResults = _.debounce(fetchSearchResults, 500);
 
     async function fetchSearchResults(searchText: string) {
         const res = await fetch("/api/search?q=" + searchText);
@@ -15,7 +18,7 @@ export default function SearchBar() {
 
     function handleChange(e: ChangeEvent<HTMLInputElement>) {
         console.log(e.target.value);
-        fetchSearchResults(e.target.value);
+        debouncedFetchSearchResults(e.target.value);
     }
     return (
         <div>
